f86bb48d0b25f94f5fdf3f5779ec98786c441fc6
CodeMolke <shubham.molke.dev@gmail.com>
2026-07-24 20:39:10 +0530

    Completed Java Basics Q01-Q04

---
+++ JAVA FOUNDATION FOR PLACEMENT/Module 1 (JAVA BASICS)/Q2_PrintName.java
@@ -0,0 +1,7 @@
+public class Q2_PrintName {
+  public static void main(String[] args) {
+    String name ="Shubham";
+    System.out.println("My Name is : " + name);
+  }
+  
+}
---
+++ JAVA FOUNDATION FOR PLACEMENT/Module 1 (JAVA BASICS)/Q3_AddTwoNumbers.java
@@ -0,0 +1,10 @@
+public class Q3_AddTwoNumbers {
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+    System.out.println("First Number  : " + a);
+    System.out.println("Second Number : " + b);
+    System.out.println("Sum           : " + (a + b));
+  }
+
+}
