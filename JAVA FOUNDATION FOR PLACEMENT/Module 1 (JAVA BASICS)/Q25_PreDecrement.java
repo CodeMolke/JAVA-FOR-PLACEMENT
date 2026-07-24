3a6cc7ea0ba4acfe0721568530210a9dcda2354f
CodeMolke <shubham.molke.dev@gmail.com>
2026-07-25 02:32:25 +0530

    Solved Q25_PreDecrement

---
+++ JAVA FOUNDATION FOR PLACEMENT/Module 1 (JAVA BASICS)/Q25_PreDecrement.java
@@ -0,0 +1,9 @@
+public class Q25_PreDecrement {
+  public static void main(String[] args) {
+    int number = 10;
+    System.out.println("Original Value     : " + number);
+    System.out.println("After PreDecrement : " + --number);
+    System.out.println("Current Value      : " + number);
+  }
+
+}
