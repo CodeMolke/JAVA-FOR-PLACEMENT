9eb9f03e090d949f10c6fa6bfcdefb61bcd32d29
CodeMolke <shubham.molke.dev@gmail.com>
2026-07-25 01:46:32 +0530

    Solved Q16_FindAverageOfThreeNumbers

---
+++ JAVA FOUNDATION FOR PLACEMENT/Module 1 (JAVA BASICS)/Q16_FindAverageOfThreeNumbers.java
@@ -0,0 +1,13 @@
+public class Q16_FindAverageOfThreeNumbers {
+  public static void main(String[] args) {
+
+    double num1 = 10;
+    double num2 = 20;
+    double num3 = 30;
+    double average = (num1 + num2 + num3) / 3;
+    System.out.println("First Number             : " + num1);
+    System.out.println("Second Number            : " + num2);
+    System.out.println("Third Number             : " + num3);
+    System.out.println("Average Of Three Numbers : " + average);
+  }
+}
