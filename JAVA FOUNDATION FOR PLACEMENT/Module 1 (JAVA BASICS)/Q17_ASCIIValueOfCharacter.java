1498c11a3d5974ee379ef91f7f6f6c8513a36453
CodeMolke <shubham.molke.dev@gmail.com>
2026-07-25 01:50:01 +0530

    Solved Q17_ASCIIValueOfCharacter

---
+++ JAVA FOUNDATION FOR PLACEMENT/Module 1 (JAVA BASICS)/Q17_ASCIIValueOfCharacter.java
@@ -0,0 +1,8 @@
+public class Q17_ASCIIValueOfCharacter {
+  public static void main(String[] args) {
+    char ch = 'A';
+    int ascii = ch;
+    System.out.println("Character   : " + ch);
+    System.out.println("ASCII Value : " + ascii);
+  }
+}
