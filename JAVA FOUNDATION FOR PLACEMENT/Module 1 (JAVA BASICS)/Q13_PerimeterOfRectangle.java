ec44df30905bbbb56eca40bb0eae71494fa45f4c
CodeMolke <shubham.molke.dev@gmail.com>
2026-07-25 01:25:09 +0530

    Solved Q13_PerimeterOfRectangle

---
+++ JAVA FOUNDATION FOR PLACEMENT/Module 1 (JAVA BASICS)/Q13_PerimeterOfRectangle.java
@@ -0,0 +1,11 @@
+public class Q13_PerimeterOfRectangle {
+  public static void main(String[] args) {
+    double length = 12;
+    double breadth = 8;
+    double perimeter = 2 * (length + breadth);
+    System.out.println("Length                 : " + length);
+    System.out.println("Breadth                : " + breadth);
+    System.out.println("Perimeter of Rectangle : " + perimeter);
+  }
+
+}
