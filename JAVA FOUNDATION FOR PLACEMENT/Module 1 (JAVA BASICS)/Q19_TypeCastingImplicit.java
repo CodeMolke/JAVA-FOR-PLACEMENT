3a2fbd9975d10640dd4ef4f03f6ff4dbbcc16556
CodeMolke <shubham.molke.dev@gmail.com>
2026-07-25 01:58:47 +0530

    Solved Q19_TypeCastingImplicit

---
+++ JAVA FOUNDATION FOR PLACEMENT/Module 1 (JAVA BASICS)/Q19_TypeCastingImplicit.java
@@ -0,0 +1,9 @@
+public class Q19_TypeCastingImplicit {
+  public static void main(String[] args) {
+    int number = 100;
+    double converted = number;
+    System.out.println("Integer Value : " + number);
+    System.out.println("Double Value  : " + converted);
+  }
+
+}
