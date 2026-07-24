716fe325358415f41537f0cb8984d32f65fdab29
CodeMolke <shubham.molke.dev@gmail.com>
2026-07-25 01:54:32 +0530

    Solved Q18_CharacterToASCII

---
+++ JAVA FOUNDATION FOR PLACEMENT/Module 1 (JAVA BASICS)/Q18_CharacterToASCII.java
@@ -0,0 +1,9 @@
+public class Q18_CharacterToASCII {
+  public static void main(String[] args) {
+    char ch = 'Z';
+    int ascii = ch;
+    System.out.println("Character  : " + ch);
+    System.out.println("ASCII Value : " + ascii);
+  }
+
+}
