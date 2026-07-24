2adc7fadb511a473f3452e935b5c19bf0b315ac3
CodeMolke <shubham.molke.dev@gmail.com>
2026-07-25 01:20:17 +0530

    Solved Q12_FahrenheitToCelsius

---
+++ JAVA FOUNDATION FOR PLACEMENT/Module 1 (JAVA BASICS)/Q12_FahrenheitToCelsius.java
@@ -0,0 +1,9 @@
+public class Q12_FahrenheitToCelsius {
+  public static void main(String[] args) {
+    double fahrenheit = 98.6;
+    double celsius = (fahrenheit - 32) * 5 / 9;
+    System.out.println("Fahrenheit : " + fahrenheit);
+    System.out.println("Celsius    : " + celsius);
+  }
+
+}
