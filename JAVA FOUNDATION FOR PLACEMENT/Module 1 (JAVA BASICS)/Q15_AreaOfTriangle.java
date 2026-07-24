b2bc405b0caa6d83ae517a32bead6f03d16c6e5b
CodeMolke <shubham.molke.dev@gmail.com>
2026-07-25 01:33:34 +0530

    Solved Q15_AreaOfTriangle

---
+++ JAVA FOUNDATION FOR PLACEMENT/Module 1 (JAVA BASICS)/Q15_AreaOfTriangle.java
@@ -0,0 +1,11 @@
+public class Q15_AreaOfTriangle {
+  public static void main(String[] args) {
+    double base = 10;
+    double height = 8;
+    double area = 0.5 * base * height;
+    System.out.println("Base                : " + base);
+    System.out.println("Height              : " + height);
+    System.out.println("Area of Triangle is : " + area);
+  }
+
+}
