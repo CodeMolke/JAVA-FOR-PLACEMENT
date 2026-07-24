61b4bdab31077d8f92c84995240658478b0d4663
CodeMolke <shubham.molke.dev@gmail.com>
2026-07-25 02:15:36 +0530

    Solved Q21_PrimitiveDataTypesDemo

---
+++ JAVA FOUNDATION FOR PLACEMENT/Module 1 (JAVA BASICS)/Q21_PrimitiveDataTypesDemo.java
@@ -0,0 +1,21 @@
+public class Q21_PrimitiveDataTypesDemo {
+  public static void main(String[] args) {
+    byte b = 10;
+    short s = 100;
+    int i = 1000;
+    long l = 100000L;
+    float f = 12.5f;
+    double d = 99.99;
+    char ch = 'A';
+    boolean isJavaFun = true;
+    System.out.println("Byte      : " + b);
+    System.out.println("Short     : " + s);
+    System.out.println("Integer   : " + i);
+    System.out.println("Long      : " + l);
+    System.out.println("Float     : " + f);
+    System.out.println("Double    : " + d);
+    System.out.println("Character : " + ch);
+    System.out.println("Boolean   : " + isJavaFun);
+  }
+
+}
