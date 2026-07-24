63ff76d51fef920ce94b3185fc47d183eaf4a91d
CodeMolke <shubham.molke.dev@gmail.com>
2026-07-25 02:29:37 +0530

    Solved Q24_PostIncrement

---
+++ JAVA FOUNDATION FOR PLACEMENT/Module 1 (JAVA BASICS)/Q24_PostIncrement.java
@@ -0,0 +1,9 @@
+public class Q24_PostIncrement {
+  public static void main(String[] args) {
+    int number = 10;
+    System.out.println("Original Value       : " + number);
+    System.out.println("After Post Increment : " + number++);
+    System.out.println("Current Value        : " + number);
+  }
+
+}
