25659aba9b4c91536e35252d567f7810d43d3dba
CodeMolke <shubham.molke.dev@gmail.com>
2026-07-25 01:28:41 +0530

    Solved Q14_PerimeterOfSquare

---
+++ JAVA FOUNDATION FOR PLACEMENT/Module 1 (JAVA BASICS)/Q14_PerimeterOfSquare.java
@@ -0,0 +1,9 @@
+public class Q14_PerimeterOfSquare {
+  public static void main(String[] args) {
+    double side = 10;
+    double perimeter = 4 * side;
+    System.out.println("Side      : " + side);
+    System.out.println("Perimeter : " + perimeter);
+  }
+
+}
