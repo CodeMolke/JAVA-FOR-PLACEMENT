a91617947670c245e9be8fbecb590b71c60be3b3
CodeMolke <shubham.molke.dev@gmail.com>
2026-07-25 02:35:04 +0530

    Solved Q26_PostDecrement

---
+++ JAVA FOUNDATION FOR PLACEMENT/Module 1 (JAVA BASICS)/Q26_PostDecrement.java
@@ -0,0 +1,9 @@
+public class Q26_PostDecrement {
+  public static void main(String[] args) {
+    int number = 10;
+    System.out.println("Original Value       : " + number);
+    System.out.println("After Post Decrement : " + number--);
+    System.out.println("Current Value        : " + number);
+  }
+  
+}
