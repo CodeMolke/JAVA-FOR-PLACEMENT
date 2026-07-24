32ec99810f90ea9d8626539833147024185a5f1f
CodeMolke <shubham.molke.dev@gmail.com>
2026-07-25 02:26:36 +0530

    Solved Q23_PreIncrement

---
+++ JAVA FOUNDATION FOR PLACEMENT/Module 1 (JAVA BASICS)/Q23_PreIncrement.java
@@ -0,0 +1,9 @@
+public class Q23_PreIncrement {
+
+  public static void main(String[] args) {
+    int number = 10;
+    System.out.println("Original Value     : " + number);
+    System.out.println("After PreIncrement : " + ++number);
+    System.out.println("Current Value      : " + number);
+  }
+}
