6bdc40bc0fcd1d857b607323ce4f647cbd565923
CodeMolke <shubham.molke.dev@gmail.com>
2026-07-25 02:08:31 +0530

    Solved Q20_TypeCastingExplicit

---
+++ JAVA FOUNDATION FOR PLACEMENT/Module 1 (JAVA BASICS)/Q20_TypeCastingExplicit.java
@@ -0,0 +1,10 @@
+public class Q20_TypeCastingExplicit {
+  public static void main(String[] args) {
+    double number = 99.99;
+    int converted = (int) number;
+    System.out.println("Double Value  : " + number);
+    System.out.println("Integer Value : " + converted);
+
+  }
+
+}
