be80593de0ab6138f76b565f07e751e019d2e46e
CodeMolke <shubham.molke.dev@gmail.com>
2026-07-25 00:39:19 +0530

    Solved Q08_AreaOfCircle

---
+++ JAVA FOUNDATION FOR PLACEMENT/Module 1 (JAVA BASICS)/Q8_AreaOfCircle.java
@@ -0,0 +1,10 @@
+public class Q8_AreaOfCircle {
+  public static void main(String[] args) {
+    double radius = 7;
+    double area = Math.PI * radius * radius;
+    System.out.println("Radius         : " + radius);
+    System.out.println("Area Of Circle : " + area);
+
+  }
+
+}
