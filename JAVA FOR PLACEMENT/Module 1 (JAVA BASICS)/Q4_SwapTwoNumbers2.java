94e8024d8088b5217c8731921500789735a2a145
CodeMolke <shubham.molke.dev@gmail.com>
2026-07-24 20:26:21 +0530

    Solved Q4_SwapTwoNumbers2.java using Arithematic Method

---
+++ JAVA FOR PLACEMENT/Module 1 (JAVA BASICS)/Q4_SwapTwoNumbers2.java
@@ -0,0 +1,17 @@
+public class Q4_SwapTwoNumbers2 {
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+    System.out.println("Before Swapping");
+    System.out.println("First Number  : " + a);
+    System.out.println("Second Number : " + b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    System.out.println("After Swapping");
+    System.out.println("First Number  : " + a);
+    System.out.println("Second Number : " + b);
+
+  }
+
+}
