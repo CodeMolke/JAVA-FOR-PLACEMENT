32d0d6785f9b6ff1d78e9083bf28e88020773deb
CodeMolke <shubham.molke.dev@gmail.com>
2026-07-25 03:47:01 +0530

    Solved Q30_FindLastDigit.java

---
+++ JAVA FOUNDATION FOR PLACEMENT/Module 1 (JAVA BASICS)/Q30_FindLastDigit.java
@@ -0,0 +1,9 @@
+public class Q30_FindLastDigit {
+  public static void main(String[] args) {
+    int num = 987;
+    int lastDigit = num % 10;
+    System.out.println("Number    : " + num);
+    System.out.println("LastDigit : " + lastDigit);
+  }
+
+}
