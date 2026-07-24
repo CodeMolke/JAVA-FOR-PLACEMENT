6d2555dded9fda480f34f2b6e97c03e3164e8cad
CodeMolke <shubham.molke.dev@gmail.com>
2026-07-25 02:22:38 +0530

    Solved Q22_UnaryOperators

---
+++ JAVA FOUNDATION FOR PLACEMENT/Module 1 (JAVA BASICS)/Q22_UnaryOperators.java
@@ -0,0 +1,11 @@
+public class Q22_UnaryOperators {
+  public static void main(String[] args) {
+    int number = 10;
+    int positive = +number;
+    int negative = -number;
+    System.out.println("Number   : " + number);
+    System.out.println("Positive : " + positive);
+    System.out.println("Negative : " + negative);
+  }
+
+}
