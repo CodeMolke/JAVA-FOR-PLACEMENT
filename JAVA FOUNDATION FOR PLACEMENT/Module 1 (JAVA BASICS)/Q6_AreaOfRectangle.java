a5f27d1a553a3aa0c035c6aff206a7ad66fe26a5
CodeMolke <shubham.molke.dev@gmail.com>
2026-07-25 00:30:18 +0530

    Solved Q06_AreaOfRectangle

---
+++ JAVA FOUNDATION FOR PLACEMENT/Module 1 (JAVA BASICS)/Q6_AreaOfRectangle.java
@@ -0,0 +1,11 @@
+public class Q6_AreaOfRectangle {
+  public static void main(String[] args) {
+    int length = 10;
+    int breadth = 5;
+    int area = length * breadth;
+    System.out.println("Length            : " + length);
+    System.out.println("Breadth           : " + breadth);
+    System.out.println("Area of Rectangle : " + area);
+  }
+
+}
