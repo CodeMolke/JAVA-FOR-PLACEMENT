943ba013781ad65379d754cafe8803563fde2ac2
CodeMolke <shubham.molke.dev@gmail.com>
2026-07-26 02:55:32 +0530

    Solved Q32_SumOfTwoDoubleNumbers

---
+++ JAVA FOUNDATION FOR PLACEMENT/Module 1 (JAVA BASICS)/Q32_SumOfTwoDoubleNumbers.java
@@ -0,0 +1,11 @@
+public class Q32_SumOfTwoDoubleNumbers {
+  public static void main(String[] args) {
+    double num1 = 10.5;
+    double num2 = 20.8;
+    double sum = num1 + num2;
+    System.out.println("First Number  : " + num1);
+    System.out.println("Second Number : " + num2);
+    System.out.println("Sum           : " + sum);
+  }
+
+}
