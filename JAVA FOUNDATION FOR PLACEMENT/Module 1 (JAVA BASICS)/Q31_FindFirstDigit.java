c6856001473220c3d0a55d98d023e008913dfa15
CodeMolke <shubham.molke.dev@gmail.com>
2026-07-26 02:51:19 +0530

    Solved Q31_FindFirstDigit

---
+++ JAVA FOUNDATION FOR PLACEMENT/Module 1 (JAVA BASICS)/Q31_FindFirstDigit.java
@@ -0,0 +1,17 @@
+public class Q31_FindFirstDigit {
+  public static void main(String[] args) {
+    int number = 9876;
+    System.out.println("Number      : " + number);
+    int temp = number;
+    int lastDigit = temp % 10;
+    temp = temp / 10;
+    int secondLastDigit = temp % 10;
+    temp = temp / 10;
+    int thirdLastDigit = temp % 10;
+    temp = temp / 10;
+    int firstDigit = temp % 10;
+    System.out.println("First Digit : " + firstDigit);
+
+  }
+
+}
