6aac529806429fffecaa93c3bd479e1019e6ec49
CodeMolke <shubham.molke.dev@gmail.com>
2026-07-26 22:31:26 +0530

    Solved Q42_KilometerToMeter

---
+++ JAVA FOUNDATION FOR PLACEMENT/Module 1 (JAVA BASICS)/Q42_KilometerToMeter.java
@@ -0,0 +1,9 @@
+public class Q42_KilometerToMeter {
+  public static void main(String[] args) {
+    double kilometer = 5.0;
+    double meter = kilometer * 1000;
+    System.out.println("Kilometer : " + kilometer);
+    System.out.println("Meter     : " + meter);
+  }
+
+}
