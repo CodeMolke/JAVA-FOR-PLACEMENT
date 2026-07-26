b7b8560a1c9e2b623472cade272cdc6c3051874a
CodeMolke <shubham.molke.dev@gmail.com>
2026-07-26 21:25:24 +0530

    Solved Q38_ReadUserInput

---
+++ JAVA FOUNDATION FOR PLACEMENT/Module 1 (JAVA BASICS)/Q38_ReadUserInput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Q38_ReadUserInput {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter Your Name : ");
+    String name = scanner.nextLine();
+    System.out.println("Hello, " + name);
+    scanner.close();
+  }
+}
