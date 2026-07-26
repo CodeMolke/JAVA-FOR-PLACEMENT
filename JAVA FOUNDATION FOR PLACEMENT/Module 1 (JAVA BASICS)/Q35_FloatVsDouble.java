2ebf6880acefe9100539459e3d39c8ebef9e5abf
CodeMolke <shubham.molke.dev@gmail.com>
2026-07-26 20:38:24 +0530

    Solved Q35_FloatVsDouble

---
+++ JAVA FOUNDATION FOR PLACEMENT/Module 1 (JAVA BASICS)/Q35_FloatVsDouble.java
@@ -0,0 +1,9 @@
+public class Q35_FloatVsDouble {
+  public static void main(String[] args) {
+    float floatValue = 12.5f;
+    double doubleValue = 12.5;
+    System.out.println("Float Value  : " + floatValue);
+    System.out.println("Double Value : " + doubleValue);
+  }
+
+}
