d19a00097ddc323f81cde4f4e9e4227544a2e658
CodeMolke <shubham.molke.dev@gmail.com>
2026-07-26 20:55:24 +0530

    Solved Q36_PrintEscapeSequences

---
+++ JAVA FOUNDATION FOR PLACEMENT/Module 1 (JAVA BASICS)/Q36_PrintEscapeSequences.java
@@ -0,0 +1,9 @@
+public class Q36_PrintEscapeSequences {
+  public static void main(String[] args) {
+    System.out.println("Hello");
+    System.out.println("Java\tProgramming");
+    System.out.println("\"Java\"");
+    System.out.println("C:\\Java");
+  }
+  
+}
